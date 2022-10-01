2157a99390bbd26b8a01beef362ec8c7dd700da4
EC2 Default User <murakamipp1028@gmail.com>
2022-10-01 01:51:37 +0000

    device/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# 会員用
+# URL /users/sign_in ...
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "users/registrations",
+  sessions: 'users/sessions'
+}
+
+# 管理者用
+# URL /admins/sign_in ...
+devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admins/sessions"
+}
+
 end
