5589d781fe7c764c7913385038f33055180e1807
EC2 Default User <murakamipp1028@gmail.com>
2022-10-17 17:26:27 +0900

    layout_run

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -52,6 +52,7 @@ class User::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました！"
     redirect_to posts_path
   end
 
